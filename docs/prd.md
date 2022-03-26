0cd2be179879c9876ab11c7945a80fe83dd00821
KELLY LUU <kellyluuu@gmail.com>
2022-03-25 20:04:45 -0500

    add basic css,js,and html

--- docs/prd.md
+++ docs/prd.md
@@ -23,42 +23,7 @@ List of stories users should experience when using your application.
 - Users can create a saved list and go back to it with local storage
 - User can delete items on saved list 
 
-## Route Tables
-
-For backend Applications you'll want to detail the different routes and types of your request your server can receive. There are three main things to define.
-
-![json](json.png "Data breakdown")
-
-- The endpoint: [the URL to which the request must be made](https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo)
-
-- The method: the type of http method the request should be
-- The response: json data
-
-
-You should also include any additional notes on any special headers that may be used and so forth.
-
-| Endpoint | Method | Response | Other |
-| -------- | ------ | -------- | ----- |
-| /item | GET | from_currency and to_currency |
-| /item | POST | return currency code, name, exchange rate, and date|
-
-
-## Component Architecture
-
-You can use the [Mermaid Markdown Syntax](https://mermaid-js.github.io/mermaid/#/flowchart) to create a chart of how the parts of your frontend website relate to each other. Units should represent components of your page. The following is an example you may see in a Single Page Application like a React App.
-
-```mermaid
-flowchart LR
-  App-->Header
-  App-->Main
-  App-->Footer
-  Main-->Router
-  Router-->Home
-  Router-->Login
-  Router-->Dashboard
-```
 
 ## User Interface Mockups
-
 ![layout](layout.png "Layout")
 
---
+++ js/script.js
@@ -0,0 +1,70 @@
+const $one = $('#one')
+const $two = $('#two')
+const $three = $('#three')
+const $sButton = $('#save')
+const $cButton = $('#convert-button')
+const $fromSearch = $('#from-currency')
+const $toSearch = $('#to-currency')
+const $fromValue = $('#from-value')
+const $toValue = $('#to-value')
+const $exchangeValue = $('#exchange-value')
+const $history = $('#history')
+const API = "UH9IHAC400APFIS3"
+const from = $fromSearch.val()
+const to = $toSearch.val()
+// const fromData = 'BTC'
+// const todData = 'USD'
+
+function convert(data){
+  $fromValue.text(`1 ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}) ` + '=')
+  $exchangeValue.text(`${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} `) 
+  $toValue.text(`${data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]})`)
+}
+
+//add on to make the UX better
+$fromSearch.on("keyup",(event) =>{
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     $cButton.click();
+    }
+  });
+
+//add on to make the UX better
+  $toSearch.on("keyup",(event) =>{
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     $cButton.click();
+    }
+  });
+
+//add on to make the UX better
+  $(document).ready(function(){
+    $.ajax(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${from}&to_currency=${to}&apikey=${API}`)
+    .then ((data)=>{
+      // Space with JSON
+      // can't not end with = sign 
+      $fromValue.text(`1 ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}) ` + '=')
+  $exchangeValue.text(`${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} `) 
+  $toValue.text(`${data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]})`)
+    })
+  })
+
+  $cButton.on('click', (event)=>{
+    event.preventDefault()
+    const from = $fromSearch.val()
+    const to = $toSearch.val()
+    $.ajax(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${from}&to_currency=${to}&apikey=${API}`)
+    .then ((data)=>{
+        $fromValue.text(`1 ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}) ` + '=')
+        $exchangeValue.text(`${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} `) 
+        $toValue.text(`${data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]})`)
+  })
+  })
+
+
+$sButton.on('click',()=>{
+  
+})
+
+
+  
