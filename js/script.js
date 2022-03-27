e1368367bd13cc62e03ae1112da19657755ac58d
KELLY LUU <kellyluuu@gmail.com>
2022-03-26 22:31:18 -0500

    added save and remove function and position css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//*----- constants -----*/
 const $one = $('#one')
 const $two = $('#two')
 const $three = $('#three')
@@ -10,16 +11,23 @@ const $toValue = $('#to-value')
 const $exchangeValue = $('#exchange-value')
 const $history = $('#history')
 const API = "UH9IHAC400APFIS3"
-const from = $fromSearch.val()
-const to = $toSearch.val()
-// const fromData = 'BTC'
-// const todData = 'USD'
+//const $textInput = $("[type='text']")
 
-function convert(data){
-  $fromValue.text(`1 ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}) ` + '=')
-  $exchangeValue.text(`${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} `) 
-  $toValue.text(`${data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]})`)
+
+// ----FUNCTION-----
+function remove(el) {
+  const element = el;
+  element.remove();
 }
+
+// $history.on("click",(e)=>{
+//   if(e.target && e.target.classList.contains('delete')){
+//     const name = e.target.dataset.name
+//   }
+// })
+
+
+
 
 //add on to make the UX better
 $fromSearch.on("keyup",(event) =>{
@@ -34,20 +42,10 @@ $fromSearch.on("keyup",(event) =>{
     if (event.keyCode === 13) {
      event.preventDefault();
      $cButton.click();
+
     }
   });
 
-//add on to make the UX better
-  $(document).ready(function(){
-    $.ajax(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${from}&to_currency=${to}&apikey=${API}`)
-    .then ((data)=>{
-      // Space with JSON
-      // can't not end with = sign 
-      $fromValue.text(`1 ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}) ` + '=')
-  $exchangeValue.text(`${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} `) 
-  $toValue.text(`${data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]})`)
-    })
-  })
 
   $cButton.on('click', (event)=>{
     event.preventDefault()
@@ -61,10 +59,16 @@ $fromSearch.on("keyup",(event) =>{
   })
   })
 
+  $sButton.on('click',(event)=>{
+    event.preventDefault()
+    const from = $fromSearch.val()
+    const to = $toSearch.val()
+    $.ajax(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${from}&to_currency=${to}&apikey=${API}`)
+    .then ((data)=>{
+      const $li = $(`<li id="${from}-${to}" class="r-list">`)
+      $li.html(`${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} ${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}
+      <br> <br> ${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} 
+      ${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]}<br> <button class="delete" " onclick="remove($('#${from}-${to}'))">Remove</button>`)
+      $history.append($li)
+  })})
 
-$sButton.on('click',()=>{
-  
-})
-
-
-  
