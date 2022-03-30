74b68b7fac3c3a70b91ee86662c38fc64e14d565
KELLY LUU <kellyluuu@gmail.com>
2022-03-30 17:14:05 -0500

    Finalize catch error message and update css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-///////////////////*----- constants -----*/
 const $contain2 = $('.container-b')
 const $three = $('#three')
 const $sButton = $('#save')
@@ -9,18 +8,12 @@ const $fromValue = $('#from-value')
 const $toValue = $('#to-value')
 const $exchangeValue = $('#exchange-value')
 const $history = $('#history')
-const $resetButton = $('#reset-button')
 const API = "UH9IHAC400APFIS3"
 const search = document.getElementById("from-currency")
 const matchLista = document.getElementById("match-list-a")
 const matchListb = document.getElementById("match-list-b")
 const code = document.querySelector("text-primary")
 const tosearch = document.getElementById("to-currency")
-const $rbutton = $('#rbutton')
-const $xbutton = $(".btn btn-outline-warning")
-const $testbox = $("#testbox")
-
-
 
 const history = JSON.parse(localStorage.getItem("history")) || []
 
@@ -30,33 +23,20 @@ const loadList = ()=>{
       $.ajax(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${element.from}&to_currency=${element.to}&apikey=${API}`)
       .then ((data)=>{
         const $li = $(`<li id="${historyId}" data-array="${historyId}" class="card-group">`)
-        $li.html(`<button id=${historyId} class="btn btn-outline-warning" onclick="remove($('#${historyId}'))"> X </button>
+        $li.html(`<button id=${historyId} class="delete" onclick="remove($('#${historyId}'))"> X </button>
         <div class="card-header">${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}</div> <div class="card-body"><h4 class="card-title">
         ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]}</h4>
         <p class="card-text">${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} <br>
         ${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]}</p>`)
         $history.append($li)
-      })
+      }).catch(()=>{
+        const $li = $(`<li id="${historyId}" data-array="${historyId}" class="card-group">`)
+        $li.html(`<div class="card-header">Error</div> <div class="card-body"><h4 class="card-title"></h4>
+        <p class="card-text">Loading Error<br>Try Again Later</p>`)
+        $history.append($li)
     })
-    }
+    })}
 
-    $history.on('click',(event)=>{
-      history.splice(event.target.id, 1)
-      localStorage.setItem("history",JSON.stringify(history) )
-    })
-
-  function remove(el) {
-    const element = el;
-    element.remove();
-  }
-
-
-  const addHistory = (from,to)=>{
-    history.push({
-      from,
-      to,
-      })
-    }
 
 
 const filterDataA = async searchText => {
@@ -85,7 +65,6 @@ const filterDataB = async searchText => {
   }
   outputHtmlB (matches);
 }
-
 
 
 const outputHtmlA = matches => {
@@ -144,8 +123,9 @@ $cButton.on('click', (event)=>{
       $fromValue.text(`1 ${data["Realtime Currency Exchange Rate"]["2. From_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]}) ` + '=')
       $exchangeValue.text(`${data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]} `) 
       $toValue.text(`${data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]} (${data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]})`)
+      $three.show()
   }).catch(()=>{
-    $fromValue.text(`invalid search`)
+    $fromValue.text(`invalid or Loading Error`)
   })
   })
 
@@ -158,5 +138,21 @@ $sButton.on('click',(event)=>{
   location.reload()
 })
 
+$history.on('click',(event)=>{
+  history.splice(event.target.id, 1)
+  localStorage.setItem("history",JSON.stringify(history) )
+})
 
+function remove(el) {
+  const element = el;
+  element.remove();
+}
+
+const addHistory = (from,to)=>{
+  history.push({
+    from,
+    to,
+    })
+  }
+$three.hide()
 loadList()
---
+++ readme.md
@@ -0,0 +1,29 @@
+
+# Product Requirements Documentation
+
+**Summary**
+| Field | Detail |
+|-------|--------|
+| Project Name | {Cryptocurrency Converter}|
+| Description | {convert between crypo and physical currency and be able to create a saved list to reference} |
+| Developers | {[List of Developers](https://www.alphavantage.co/documentation/)} |
+| Live Website | {GitHub} |
+| Repo | {[Link to git repository](https://github.com/kellyluuu/Cryptocurrency-Converter.git)} |
+
+## Problem Being Solved and Target Market
+calculate the value of any crypto and physical currency. Create a saved list of conversion user are interested in. 
+
+## User Stories
+
+List of stories users should experience when using your application.
+
+- Users should be able to see the site on desktop and mobile
+- User will see the default of conversion from BTC to USD
+- User can change the currency to convert from and to 
+- Users can create a saved list and go back to it with local storage
+- User can delete items on saved list 
+
+
+## User Interface Mockups
+![layout](docs/layout.png "Layout")
+
